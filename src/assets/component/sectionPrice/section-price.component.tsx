eb67c16db604c862a98533bd8f9a035a4080e891
DaniRC <Edanielrivero@hotmail.com>
2024-10-19 21:20:20 -0300

    update mistakes

--- src/assets/component/sectionPrice/section-price.component.tsx
+++ src/assets/component/sectionPrice/section-price.component.tsx
@@ -1,7 +1,31 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useRef, useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { userForm } from "../../type";
+import emailjs from "emailjs-com";
+
+const ID_SERVICE = import.meta.env.VITE_ID_SERVICE!;
+const ID_TEMPLATE = import.meta.env.VITE_ID_TEMPLATE!;
+const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY!;
 
 const SectionPrice: FunctionComponent<Prop>=({className})=>{
+  const [user, setUser] = useState<userForm>({name:'', email: '', message: ''})
+  const form = useRef()
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>)=>{
+    e.preventDefault();
+    
+    emailjs.sendForm(ID_SERVICE, ID_TEMPLATE, form.current, PUBLIC_KEY)
+    .then((response) => {
+      console.log(PUBLIC_KEY)
+      console.log('Email sent successfully:', response.status, response.text);
+      alert('enviado')
+      setUser({ name: '', email: '', message: '' }); // Limpiar el formulario
+    })
+    .catch((error) => {
+      console.error('Failed to send email:', error);
+    });
+};
+  
     return(
       <section className={className} >
          <Helmet>
@@ -16,10 +40,10 @@ const SectionPrice: FunctionComponent<Prop>=({className})=>{
       />
       <div className="form-container">
         <h3 className="title-h3">Solicitar un Presupuesto</h3>
-        <form>
-          <input type="text" placeholder="Nombre" required aria-label="Nombre"/>
-          <input type="email" placeholder="Email" required aria-label="Email"/>
-          <textarea placeholder="Mensaje" rows={4} required aria-label="Mensaje"/>
+        <form onSubmit={sendEmail}>
+          <input type="text" placeholder="Nombre" name="name" value={user.name} onChange={(e) => setUser({...user, name: e.target.value})} required aria-label="Nombre"/>
+          <input type="email" placeholder="Email" name="email" value={user.email} onChange={(e)=> setUser({...user, email: e.target.value})} required aria-label="Email"/>
+          <textarea placeholder="Mensaje" name="message" value={user.message} onChange={(e) => setUser({...user, message: e.target.value})} rows={8} required aria-label="Mensaje"/>
           <button type="submit">Enviar</button>
         </form>
       </div>
---
+++ src/assets/type.ts
@@ -0,0 +1,5 @@
+export interface userForm{
+    name: string,
+    email: string,
+    message: string,
+}
